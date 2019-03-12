1a3c06c62fde93a873fc82da7799bd0203c5c738
Bruno Pincho <b.pincho@runtime-revolution.com>
2019-03-12 11:26:58 +0000

    Guard against file not found exception

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from flask import (Flask, send_file)
-from subprocess import call
+from flask import (Flask, send_file, abort)
+from subprocess import call, CalledProcessError
 
 app = Flask(__name__)
 
@@ -11,14 +11,17 @@ def downloadPdf(api_url):
 		json_url = api_url.split("version")[0] + '?version' + api_url.split("version")[1]
 	else:
 		json_url = api_url
-		
-	call(["java","-jar","swagger2markup-cli-1.3.3.jar","convert","-i",
-		 json_url,"-f","DAA_API"])
-	print("markup to pdf convertion \n")
-	call(["asciidoctor-pdf", "DAA_API.adoc"])
-	print("PDF Generated \n")
-	path = "DAA_API.pdf"
-	return send_file(path, as_attachment=True)
+	try:	
+		call(["java","-jar","swagger2markup-cli-1.3.3.jar","convert","-i",
+			 json_url,"-f","DAA_API"])
+		print("markup to pdf convertion \n")
+		import pdb;pdb.set_trace()
+		call(["asciidoctor-pdf", "DAA_API.adoc"])
+		print("PDF Generated \n")
+		path = "DAA_API.pdf"
+		return send_file(path, as_attachment=True)
+	except FileNotFoundError:
+		return abort(418)
 
 port = int(os.environ.get('PORT', 5000))
 app.run(debug=True, host='0.0.0.0', port=port)
